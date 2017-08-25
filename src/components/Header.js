6c0109e09e26d4f25e45842365124a11ad519b39
Leo Generali <leogen17@gmail.com>
2017-08-25 17:05:52 -0400

    Update header to include link to personal site; update shared sass to include link wrapper

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,7 @@ class Header extends Component {
 			<header className="header">
 				<div className="header__container">
 					<h1>Better Gradient Tool</h1>
+					<p className="created-with">Created with <span className="heart">♥</span> by <span className="link-wrapper"><a href="http://www.leogenerali.com/">Leo Generali</a></span></p>
 				</div>
 			</header>
 		);
