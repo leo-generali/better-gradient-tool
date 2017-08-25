3a8c3f7ef1fb677d67f040c1cd2b6cbc0c69bd53
Leo Generali <leogen17@gmail.com>
2017-08-24 22:26:21 -0400

    Add support for copying the gradients directly from the cards

--- src/components/GradientCard.js
+++ src/components/GradientCard.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 
 class GradientCard extends Component {
+	copyCSS(css) {
+		//This is a SUPER hacky way of copy the gradient over. Will need to look
+		//into better ways. 
+		//
+		//
+		//Currently, this creates a text area that is a child of the body. It selects
+		//the newly created text, creates a text area under the body, copies it, and
+		//then removes the text area... Sneaky!
+
+		const tempTextArea = document.createElement('textarea');
+		tempTextArea.value = css.background;
+		document.body.appendChild(tempTextArea);
+		console.log(css.background);
+		tempTextArea.select();
+		document.execCommand("copy");
+		document.body.removeChild(tempTextArea);
+	}
+
 	render() {
 
 		const style = {
@@ -10,7 +28,7 @@ class GradientCard extends Component {
 		const name = this.props.name;
 
 		return(
-			<div style={style} className="card">
+			<div style={style} className="card card__gradient" onClick={() => this.copyCSS(style)} >
 				{name}
 			</div>
 		);
