b570f16af49cab73bfd5532ebe00157720257f58
Leo Generali <leogen17@gmail.com>
2017-08-29 12:13:33 -0400

    Add support for sad emojies

--- src/helpers/notifications.js
+++ src/helpers/notifications.js
@@ -1,18 +1,23 @@
-export function getRandomEmoji() {
-    const emojis = ["👍", "❤", "😍", "😊", "🔥", "⚡", "👌","💪", "✅", "💥"]
+export function getRandomEmoji(happy) {
+	const emojis = happy ? 
+		["👍", "❤", "😍", "😊", "🔥", "⚡", "👌","💪", "✅", "💥"] : 
+		["😭", "😩", "😢", "👎", "😐"];
+
     const num = Math.floor((Math.random() * emojis.length));
     const emoji = emojis[num];
     return emoji;
 }
 
 export function createCopyNotification(name) {
-    return `You've succesfully copied ${name}! ${getRandomEmoji()}`;
+    return `You've succesfully copied ${name}! ${getRandomEmoji(true)}`;
 }
 
 export function createFaveNotification(name, status) {
-    return status ?  `You've unfavorited ${name}!` : `You've favorited ${name}! ${getRandomEmoji()}`;
+    return status ?  
+    	`You've  unfavorited ${name}! ${getRandomEmoji(!status)}` : 
+    	`You've favorited ${name}! ${getRandomEmoji(!status)}`;
 }
 
 export function createNoFaveNotification() {
-	return "You haven't favorited any gradients yet!";
+	return `You haven't favorited any gradients yet! ${getRandomEmoji(false)}`;
 }
