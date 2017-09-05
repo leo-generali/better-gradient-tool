3f9a5f0c25c801913faae48dbc4837817c9589d3
Leo Generali <leogen17@gmail.com>
2017-09-05 16:13:54 -0400

    Update styling on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends Component {
 			<header className="header">
 				<div className="header__container">
 					<h1 className="bgt-logo">Better Gradient Tool</h1>
-					<p className="created-with">Created with <span className="heart">♥</span> by <span className="link-wrapper"><a href="http://www.leogenerali.com/">Leo Generali</a></span></p>
+					<p className="created-with">Created with <span className="heart">♥</span> by <a href="http://www.leogenerali.com/">Leo Generali</a></p>
 				</div>
 				<div className="header__container header__container--lower">
 					<p className="header-mods">Modifiers:</p><input className="checkbox" type="checkbox" checked={checked} onClick={this.props.updatePrefix}/><p>Prefixes</p>
